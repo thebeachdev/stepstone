5076291872d9844b8253daf97913576f54d14d51
James Hall <greatwolf3d@gmail.com>
2017-06-16 10:53:08 -0700

    Working on sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CompanyService } from './_services/index';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  public rows:Array<any> = [];
+  public columns: Array<any> = [
+    {
+      title: 'Name', name: 'name', filtering:
+      {
+        filterString: '', placeholder: 'Filter by name'
+      }},
+      {
+        title: 'Revenue',
+        name: 'revenue',
+        sort: false,
+        filtering: {
+          filterString: '', placeHolder: 'Filter by Revenue'
+        },
+        {
+          title: 'Year',
+          name: 'year'
+        },
+        {
+          title: 'Industry',
+          name: 'industry'
+      }
+    ];
+  public config: any = {
+    paging: false,
+    sorting: {columns: this.columns },
+    filtering: { filterString: ''}
+  };
 
   private companies: any = { };
 
@@ -18,6 +47,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {// this creates an error I don't have time to fix right now.
     console.log("in init");
+    this.onChangeTable(this.config);
     this.cs.getCompanies().subscribe(
       data => {
         this.companies = data.companies.map(({ id, name, revenue, year, industry }) => ({ id, name, revenue, year, industry }))
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AppComponent } from './app.component';
 import { CompanyService } from './_services/index';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpModule } from '@angular/http';
+import { OrderByPipe } from './_pipes/order-by.pipe';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
 @NgModule({
   declarations: [
     AppComponent,
+    OrderByPipe,
   ],
   imports: [
     HttpModule,
     BrowserModule,
+    Ng2TableModule,
     AppRoutingModule
   ],
   providers: [CompanyService],
