cb9de0babe46f0a1e0d41af2dfe2fe9b8dec778a
James Hall <greatwolf3d@gmail.com>
2017-06-16 00:09:12 -0700

    Added a little routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { CompanyResolver } from './_services/index';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    resolve: {
+      company: CompanyResolver
+    }
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [CompanyResolver]
+})
+export class AppRoutingModule { }
