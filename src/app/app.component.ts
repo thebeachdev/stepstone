961fa2868d67b1a5cf0fe281cde68b765143f340
James Hall <greatwolf3d@gmail.com>
2017-06-16 07:39:43 -0700

    Getting ready for list view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { CompanyService } from './_services/index';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private companies: any = { };
+  constructor(private route: ActivatedRoute, private cs: CompanyService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe( data => {
+      this.companies = data.companies.map(({ id, name, revenue, year, industry })=> ({ id, name, revenue, year, industry }))
+    }, error => {
+      console.log(`Something went wrong: ${error}`);
+    });
+  }
 }
