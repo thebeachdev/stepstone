dc353bfff42d12799e41b3ab2f62d9e3cfbc7ed5
James Hall <greatwolf3d@gmail.com>
2017-06-16 00:04:13 -0700

    added some starting stuff for module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CompanyService } from './_services/index';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [CompanyService],
   bootstrap: [AppComponent]
