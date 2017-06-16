3515f96f5617ae37fb4098d635ef3578e15e67c3
James Hall <greatwolf3d@gmail.com>
2017-06-15 23:45:43 -0700

    It would be great if we could use that service lol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { CompanyService } from './_services/index';
 @NgModule({
   declarations: [
     AppComponent,
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CompanyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
