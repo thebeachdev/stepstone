af9bca9bd241daa37864e50e4f987c8e4b4be382
James Hall <greatwolf3d@gmail.com>
2017-06-16 08:53:07 -0700

    got data showing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,23 @@ import 'rxjs/Rx';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   private companies: any = { };
-  constructor(private route: ActivatedRoute, private cs: CompanyService) { }
+
+  constructor(private route: ActivatedRoute, private cs: CompanyService) {
+    this.cs = cs;
+ }
 
   ngOnInit() {
-    this.route.data.subscribe( data => {
-      this.companies = data.companies.map(({ id, name, revenue, year, industry })=> ({ id, name, revenue, year, industry }))
-    }, error => {
-      console.log(`Something went wrong: ${error}`);
-    });
+    console.log("in init");
+    this.cs.getCompanies().subscribe(
+      data => {
+        this.companies = data.companies.map(({ id, name, revenue, year, industry }) => ({ id, name, revenue, year, industry }))
+      },
+      error => {
+        console.log(error);
+        return error;
+      }
+    );
   }
 }
